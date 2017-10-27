40dc33e92bb89f2ace1d01d0915eb3245eda53e4
Ramesh <rameshh@unitedtechno.com>
2017-10-27 09:44:15 +0530

    sdsd

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApplication2
 {
     static class Program
     {
-        /// d<summary>
+        /// dwdwdwdwd<summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
